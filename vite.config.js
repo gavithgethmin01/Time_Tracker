6c78030daac87434a40bb0c4ea4f05beded7f53d
GavithGethmin <150597528+gavithgethmin01@users.noreply.github.com>
2026-03-16 17:02:03 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,6 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: "/Hour-Tracker/",
+  // Change the line below from "/Hour-Tracker/" to just "/"
+  base: "/", 
 });
